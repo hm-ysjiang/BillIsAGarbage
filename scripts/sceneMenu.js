6e21633c284e0fcf86a991f9a53859adcad23f58
hm-ysjiang <hmysjiang.git@gmail.com>
2019-10-19 01:44:20 +0800

    Menu scene almost done

--- scripts/sceneMenu.js
+++ scripts/sceneMenu.js
@@ -15,8 +15,30 @@ class SceneMenu {
 			}
 		});*/
 		SceneMenu.manager = new BABYLON.GUI.GUI3DManager(SceneMenu.scene);
-		SceneMenu.anchor = new BABYLON.TransformNode("");
 		
+		let panelTxt = new BABYLON.GUI.PlanePanel();
+		// panelTxt.
+		
+		let panelBtn = new BABYLON.GUI.PlanePanel();
+		panelBtn.margin = 0.2;
+		SceneMenu.manager.addControl(panelBtn);
+		panelBtn.position.y = -0.5;
+		panelBtn.position.z = -2;
+		let btnPlay = new BABYLON.GUI.HolographicButton("orientation");
+		panelBtn.addControl(btnPlay);
+		btnPlay.text = "Play";
+		btnPlay.onPointerDownObservable.add(()=>{
+			scenePhase = 2;
+			console.log("Switch to Game Scene");
+		});
+		let btnTutorial = new BABYLON.GUI.HolographicButton("orientation");
+		panelBtn.addControl(btnTutorial);
+		btnTutorial.text = "Tutorial";
+		btnTutorial.onPointerDownObservable.add(()=>{
+			scenePhase = 1;
+			console.log("Switch to Tutorial Scene");
+		});
+		panelBtn.columns = 2;
 	}
 }
 
