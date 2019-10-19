52efe869267794819766378071cf10f02c46c15d
gamerslouis <gamerslouis@gmail.com>
2019-10-20 04:33:46 +0800

    Implement movable menu background

--- scripts/sceneMenu.js
+++ scripts/sceneMenu.js
@@ -3,15 +3,15 @@ class SceneMenu {
 	static camera = null;
 	static manager = null;
 
-	static setup(){
+	static setup() {
 		SceneMenu.manager = new BABYLON.GUI.GUI3DManager(SceneMenu.scene);
-		
+
 		let scene = SceneMenu.scene;
 		let cam = SceneMenu.camera;
 		let manager = SceneMenu.manager;
-		
+
 		cam.inputs.clear();
-		
+
 		let adv = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("TextUI");
 		let panelTxt1 = new BABYLON.GUI.Rectangle();
 		let panelTxt2 = new BABYLON.GUI.Rectangle();
@@ -22,14 +22,14 @@ class SceneMenu {
 		panelTxt1.isVertical = true;
 		panelTxt1.fontSize = 64;
 		panelTxt1.thickness = 0;
-        panelTxt1.top = "50";
+		panelTxt1.top = "50";
 		panelTxt1.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
 		panelTxt2.width = "600px";
 		panelTxt2.height = "100px";
 		panelTxt2.isVertical = true;
 		panelTxt2.fontSize = 64;
 		panelTxt2.thickness = 0;
-        panelTxt2.top = "200";
+		panelTxt2.top = "200";
 		panelTxt2.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
 		let txt1 = new BABYLON.GUI.TextBlock("Welcome");
 		let txt2 = new BABYLON.GUI.TextBlock("Title");
@@ -39,8 +39,8 @@ class SceneMenu {
 		txt2.text = "Bill Is A Garbage";
 		txt1.color = "white";
 		txt2.color = "white";
-		
-		
+
+
 		let btnMaterial = new BABYLON.StandardMaterial("btnMaterial", scene);
 		btnMaterial.diffuseColor = new BABYLON.Color3(0.3, 0.3, 0.3);
 		btnMaterial.specularColor = new BABYLON.Color3(0.3, 0.3, 0.3);
@@ -57,8 +57,8 @@ class SceneMenu {
 		panelBtn.addControl(btnPlay);
 		btnPlay.text = "Play";
 		btnPlay.mesh.material = btnMaterial;
-		btnPlay.onPointerDownObservable.add(()=>{
-			if (SceneMenu.hasFocus()){
+		btnPlay.onPointerDownObservable.add(() => {
+			if (SceneMenu.hasFocus()) {
 				scenePhase = 2;
 				cam.detachControl();
 				SceneMain.camera.attachControl(canvas, true);
@@ -69,8 +69,8 @@ class SceneMenu {
 		panelBtn.addControl(btnTutorial);
 		btnTutorial.text = "Tutorial";
 		btnTutorial.mesh.material = btnMaterial;
-		btnTutorial.onPointerDownObservable.add(()=>{
-			if (SceneMenu.hasFocus()){
+		btnTutorial.onPointerDownObservable.add(() => {
+			if (SceneMenu.hasFocus()) {
 				scenePhase = 1;
 				cam.detachControl();
 				SceneTutorial.camera.attachControl(canvas, true);
@@ -78,13 +78,19 @@ class SceneMenu {
 			}
 		});
 		panelBtn.columns = 2;
+
+		SceneMenu.scene.registerBeforeRender(() => {
+			if (SceneMenu.skybox)
+				SceneMenu.skybox.rotate(new BABYLON.Vector3(0.2, 1, 0), Math.PI / 3000);
+		});
+
 	}
-	
-	static hasFocus(){
+
+	static hasFocus() {
 		return scenePhase != 1 && scenePhase != 2;
 	}
-	
-	static setupSkybox(){
+
+	static setupSkybox() {
 		let scene = SceneMenu.scene;
 		let skybox = BABYLON.Mesh.CreateBox("skyBox", 7000.0, scene);
 		let skyMaterial = new BABYLON.StandardMaterial("skyBox", scene);
@@ -94,6 +100,7 @@ class SceneMenu {
 		skyMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
 		skybox.infiniteDistance = true;
 		skybox.material = skyMaterial;
+		SceneMenu.skybox = skybox
 	}
 }
 
