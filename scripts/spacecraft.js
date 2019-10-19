ca3796287814ab5794610c331827c40461c89d20
gamerslouis <gamerslouis@gmail.com>
2019-10-19 10:13:54 +0800

    Implement simple spacecraft contrl

--- scripts/spacecraft.js
+++ scripts/spacecraft.js
@@ -2,27 +2,52 @@ class Spacecraft {
     constructor(scene, assetsManager, camera) {
         this.scene = scene;
         this.camera = camera;
-        this.spacecraft = null;
+        this.spacecraft = new BABYLON.TransformNode();
 
-        assetsManager.addMeshTask('meshs', "", "mesh/", "aero4.obj").onSuccess
-            = (function (task) {
-                this.spacecraft = task.loadedMeshes[0];
-            }).bind(this)
+        assetsManager.addMeshTask('meshs', "", "mesh/", "aero4.obj").onSuccess = (function (task) {
+
+            task.loadedMeshes.forEach(mesh => {
+                // leave meshes already parented to maintain model hierarchy:
+                if (!mesh.parent) {
+                    mesh.parent = this.spacecraft
+                }
+            });
+
+        }).bind(this)
     }
 
     update() {
-            console.log(this.spacecraft)
         if (this.spacecraft != null) {
-            console.log("trigger~")
             if (this.scene.inputMap["w"]) {
-                console.log("trigger")
-                let direction = BABYLON.Vector3.Normalize(
-                    BABYLON.Vector3.TransformCoordinates(
-                        BABYLON.Vector3(1, 0, 0),
-                        this.spacecraft.getWorldMatrix()));
-                this.spacecraft.position.add(direction);
+                this.spacecraft.translate(
+                    new BABYLON.Vector3(0, 0, 1)
+                    , 10
+                    , BABYLON.Space.WORLD);
             }
         }
-
+        if (this.spacecraft != null) {
+            if (this.scene.inputMap["a"]) {
+                this.spacecraft.translate(
+                    new BABYLON.Vector3(-1, 0, 0)
+                    , 10
+                    , BABYLON.Space.WORLD);
+            }
+        }
+        if (this.spacecraft != null) {
+            if (this.scene.inputMap["s"]) {
+                this.spacecraft.translate(
+                    new BABYLON.Vector3(0, 0, -1)
+                    , 10
+                    , BABYLON.Space.WORLD);
+            }
+        }
+        if (this.spacecraft != null) {
+            if (this.scene.inputMap["d"]) {
+                this.spacecraft.translate(
+                    new BABYLON.Vector3(1, 0, 0)
+                    , 10
+                    , BABYLON.Space.WORLD);
+            }
+        }
     }
 }
